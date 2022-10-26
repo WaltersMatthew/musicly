eb0471c22e9b7d8fc61fae3ee09876ba702e87d7
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-26 14:08:26 -0700

    add teacher map on search

--- src/components/pages/Search.jsx
+++ src/components/pages/Search.jsx
@@ -1,21 +1,55 @@
-export default function Search(){
-    return(
-        <form class="w-full max-w-sm mx-auto mt-20">
-            <div class="flex items-center border-b border-teal-500 py-2">
-                <label htmlFor="search"></label>
-                <input 
-                    className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
-                    name="search"
-                    type="text" 
-                    placeholder="Trombone" 
-                />
+import axios from 'axios'
+import { useState } from 'react'
 
-                <button 
-                    className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit"
-                >
-                    Search
-                </button>
+export default function Search(){
+    const [teachers, setTeachers] = useState([])
+    const getData = async e => {
+        e.preventDefault()
+        try {
+            const response = await axios.get('/api/teachers')
+            setTeachers(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    const teacherMap = teachers.map(teacher => {
+        return(
+            <div key={teacher.id}>
+               <p>{teacher.name}</p>
+               <p>{teacher.email}</p> 
+               <p>{teacher.bio}</p>
+               <p>Years of experience: {teacher.years_experience}</p>
             </div>
-        </form>
+        )
+    })
+
+
+
+    return(
+        <div>
+            <form className="w-full max-w-sm mx-auto mt-20">
+                <div className="flex items-center border-b border-teal-500 py-2">
+                    <label htmlFor="search"></label>
+                    <input 
+                        className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
+                        name="search"
+                        type="text" 
+                        placeholder="Trombone" 
+                        />
+
+                    <button 
+                        onClick={getData}
+                        className="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit"
+                        >
+                        Search
+                    </button>
+                </div>
+            </form>
+
+            <div>
+                {teacherMap}
+            </div>
+        </div>
     )
 }

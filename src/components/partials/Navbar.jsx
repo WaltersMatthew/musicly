ca449f0cff18abfc691405055137fdd8656d4b5a
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-25 19:27:23 -0700

    update title and favicon

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 export default function Navbar(){
     return(
         <nav>
+            <p>Musicly</p>
             <Link to=''>Home</Link>
             <> | </>
             <Link to='/search'>Search Teachers</Link>

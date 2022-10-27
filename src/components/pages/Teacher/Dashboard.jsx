ff457e19b3de38bc0253f272248e8a12b3101f3b
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-27 15:18:10 -0700

    htmlFor fix

--- src/components/pages/Teacher/Dashboard.jsx
+++ src/components/pages/Teacher/Dashboard.jsx
@@ -25,7 +25,7 @@ export default function Dashboard(){
                         </li>
                     </ul>
                     <div className="flex justify-end">
-                        <label for="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
+                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
                         <input type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                     </div>
                 </div>
@@ -56,7 +56,7 @@ export default function Dashboard(){
                         </li>
                     </ul>
                     <div className="flex justify-end">
-                        <label for="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
+                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
                         <input type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                     </div>
                 </div>

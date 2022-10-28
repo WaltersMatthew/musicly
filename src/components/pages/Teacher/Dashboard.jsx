8fd383369eada3d085dd7d8d1245c402f48be97d
waltersmatthew <waltersmatthew88@gmail.com>
2022-10-28 12:28:51 -0700

    working on dashboard

--- src/components/pages/Teacher/Dashboard.jsx
+++ src/components/pages/Teacher/Dashboard.jsx
@@ -4,95 +4,158 @@ import { useNavigate, useParams } from "react-router-dom"
 
 
 export default function Dashboard(){
-    const [studentName, setStudentName] = useState('')
-    
+    const [inquiries, setInquiries] = useState([])
+    const [checked, setChecked] = useState(false)
     const { id } = useParams()
     const navigate = useNavigate()
-
+    // let inquiries = []
     useEffect(()=>{
         const getInquiries = async () =>{
             try{
                 const response = await axios.get(`/api/inquiries/`)
-                console.log(response.data)
+                setInquiries(response.data)
             }catch(err){
                 console.log(err)
             }
         }
         getInquiries()
+//working out this function to post to the api on checkbox click
+        const updateViewed = async () => {
+            try {
+                await axios.post(`api/inquiries/`)
+            } catch (error) {
+                console.log(error)
+            }
+        }
     },[])
-
-    const inquiryMap = response.data.map(inquiry =>{
-        return(
-            <div>
-
-            </div>
-        )
-
+    // console.log(inquiries)
+    
+    const checkedBox = e => {
+        setChecked(true)
+        console.log(e)
     }
-    )
+
+    const unreadMap = inquiries.map(inquiry =>{
+        if (inquiry.preferred_teacher == id && inquiry.viewed == false){
+            return(
+                <div key={`key${inquiry.id}`}>
+                <div className="block p-6 max-w-sm mx-auto bg-white rounded-lg border border-gray-200 shadow-md  hover:bg-gray-100">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                        {inquiry.student_name}
+                    </h5>
+                    
+                    <ul className="font-normal text-gray-700">
+                        <li>
+                            <span className='font-bold'>
+                                Email: </span>
+                            {inquiry.email}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Phone: </span>
+                            {inquiry.phone_number}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Instrument: </span>
+                            {inquiry.instrument}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Availability: </span> 
+                            {inquiry.availability}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>Inquiry content: </span>{inquiry.content}
+                        </li>
+                    </ul>
+
+                    <div className="flex justify-end">
+                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
+                        
+                        <input 
+                            type="checkbox" 
+                            onChange={e => checkedBox()}
+                            className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    </div>
+                </div>  
+            </div> 
+            )
+        }
+    })
+
+    const readMap = inquiries.map(inquiry =>{
+        if (inquiry.preferred_teacher == id && inquiry.viewed == true){
+            return(
+                <div key={`key${inquiry.id}`}>
+                <div className="block p-6 max-w-sm mx-auto bg-white rounded-lg border border-gray-200 shadow-md  hover:bg-gray-100">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                        {inquiry.student_name}
+                    </h5>
+                    
+                    <ul className="font-normal text-gray-700">
+                        <li>
+                            <span className='font-bold'>
+                                Email: </span>
+                            {inquiry.email}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Phone: </span>
+                            {inquiry.phone_number}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Instrument: </span>
+                            {inquiry.instrument}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>
+                                Availability: </span> 
+                            {inquiry.availability}
+                        </li>
+
+                        <li>
+                            <span className='font-bold'>Inquiry content: </span>{inquiry.content}
+                        </li>
+                    </ul>
+
+                    <div className="flex justify-end">
+                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
+                        
+                        <input type="checkbox" value="" checked className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                    </div>
+                </div>  
+            </div> 
+            )
+        }
+    })
+
+
 
     return(  
         <div>
             <div className="m-5">
                 <h1 className="text-2xl flex justify-center mt-5 shadow-xl border-2"><span className="p-4">Uncontacted</span></h1>
             </div>
+            
             <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-1 flex wrap justify-evenly m-10">
-                <div className="block p-6 max-w-sm mx-auto bg-white rounded-lg border border-gray-200 shadow-md  hover:bg-gray-100">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Matthew Walters</h5>
-                    <ul className="font-normal text-gray-700">
-                        <li>
-                        <span className='font-bold'>Email:</span> surfingdrums@gmail.com
-                        </li>
-                        <li>
-                        <span className='font-bold'>Phone:</span> 909-497-4670
-                        </li>
-                        <li>
-                        <span className='font-bold'>Instrument:</span> Guitar
-                        </li>
-                        <li>
-                        <span className='font-bold'>Availability:</span> F/Sat/Sun 12p-4p
-                        </li>
-                        <li>
-                        <span className='font-bold'>Inquiry content:</span> I really wanna impress the ladies, I have no experience, but really wanna get good
-                        </li>
-                    </ul>
-                    <div className="flex justify-end">
-                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
-                        <input type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    </div>
-                </div>
-                
+                {unreadMap}
             </div>    
 
             <div>
                 <h1 className="text-2xl flex justify-center m-5 border-2 p-4 shadow-xl"><span>Contacted</span></h1>
             </div>
+            
             <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-1 flex wrap justify-evenly m-10">
-            <div className="block p-6 max-w-sm mx-auto bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">Matthew Walters</h5>
-                    <ul className="font-normal text-gray-700">
-                        <li>
-                        <span className='font-bold'>Email:</span> surfingdrums@gmail.com
-                        </li>
-                        <li>
-                        <span className='font-bold'>Phone:</span> 909-497-4670
-                        </li>
-                        <li>
-                        <span className='font-bold'>Instrument:</span> Guitar
-                        </li>
-                        <li>
-                        <span className='font-bold'>Availability:</span> F/Sat/Sun 12p-4p
-                        </li>
-                        <li>
-                        <span className='font-bold'>Inquiry content:</span> I really wanna impress the ladies, I have no experience, but really wanna get good
-                        </li>
-                    </ul>
-                    <div className="flex justify-end">
-                        <label htmlFor="checked-checkbox" className="mr-2 text-sm font-medium text-gray-900">Contacted</label>
-                        <input type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                    </div>
-                </div>
-                
+                {readMap}
             </div>
         </div>
 

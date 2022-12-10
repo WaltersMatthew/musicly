1d59871e9ebfe2ae99ac0b0f99f567a91b041025
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-10 15:56:47 -0800

    chore: fix stretched images with screen targets

--- src/components/partials/Navbar.jsx
+++ src/components/partials/Navbar.jsx
@@ -4,18 +4,18 @@ export default function Navbar(){
     return(
         <nav className='bg-white'>
         <ul className='flex justify-between'>
-            <li className='m-3'>
+            <li className='ml-3 mt-3'>
                 <Link 
                     className='inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white transition duration-150 ease-in-out'
                     to='/search'
                     >
-                    Search Teachers
+                    <p className=''>Search Teachers</p>
                 </Link>
             </li>
 
-            <li className='m-3 flex inline'>
+            <li className='mr-3 mt-3 flex inline'>
                 <Link
-                    className='flex inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4 text-2xl font-bold transition duration-150 ease-in-out' 
+                    className='my-3 sm:my-0 flex inline-block border border-white rounded hover:border-gray-200 text-blue-500 hover:bg-gray-200 py-2 px-4 text-2xl font-bold transition duration-150 ease-in-out' 
                     to='/'
                     >
                     <img 
@@ -33,7 +33,7 @@ export default function Navbar(){
                     className='inline-block border border-blue-500 rounded py-2 px-4 bg-blue-500 hover:bg-blue-700 text-white transition duration-150 ease-in-out'
                     to='/teacher/1/dashboard'
                 >
-                    Teacher Dashboard
+                    Lesson Home
                 </Link>
             </li>
         </ul>
